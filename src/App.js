b02a5dd102b29b4f8178d92638317b7f70269bb9
Juan Quintero <quinterojuan0246@gmail.com>
2023-11-17 03:15:27 -0300

    feat: ProductListComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import { ProductListComponent } from "./components/productList/productList";
 import { ProductSelectorComponent } from "./components/productSelector/productSelector";
 import { PurchaseDetailsComponent } from "./components/purchaseDetails/purchaseDetails";
 
 function App() {
+  //list of products
+  const [onSale, setOnSale] = useState([]);
+  const [inventory, setInventory] = useState([
+    { id: 1, name: "Item 1", price: 100, quantity: 1 },
+    { id: 2, name: "Item 2", price: 200, quantity: 2 },
+  ]);
+
   return (
     <div className="App">
-      <ProductListComponent />
+      <ProductListComponent inventory={inventory} setInventory={setInventory} />
       <ProductSelectorComponent />
       <PurchaseDetailsComponent />
     </div>
--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -1,3 +1,45 @@
-export const ProductListComponent = () => {
-  return <div className="ProductListComponent"></div>;
+import { useEffect } from "react";
+import "./productList.css";
+
+export const ProductListComponent = ({ inventory, setInventory }) => {
+  return (
+    <div className="ProductListComponent">
+      <div className="tbl-header">
+        <table>
+          <thead>
+            <tr>
+              <th>cantidad</th>
+              <th>descripcion</th>
+              <th>subtotal</th>
+              <th>total</th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="tbl-content">
+        <table>
+          <tbody>
+            {inventory.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>
+                    <button>&lt;</button>
+                    {item.quantity}
+                    <button>&gt;</button>
+                  </td>
+                  <td>{item.name}</td>
+                  <td className="number-aling">{item.price}</td>
+                  <td className="number-aling"></td>
+                  <td>
+                    <button>quitar</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
