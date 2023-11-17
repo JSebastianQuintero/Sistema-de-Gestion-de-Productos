97f37495a2515e565f40d788642bf71cea4dcbb6
Juan Quintero <quinterojuan0246@gmail.com>
2023-11-17 17:31:00 -0300

    feat: functions for PurchaseDetailsComponent

--- src/utils/products.js
+++ src/utils/products.js
@@ -25,3 +25,13 @@ export function removeProduct(itemID, inventory, setInventory) {
   const newInventory = inventory.filter((item) => item.id !== itemID);
   setInventory(newInventory);
 }
+
+// Desc: Utility functions for purchase details
+
+export function countItems(inventory) {
+  return inventory.reduce((acc, item) => acc + item.quantity, 0);
+}
+
+export function getTotalPrice(inventory) {
+  return inventory.reduce((acc, item) => acc + item.price * item.quantity, 0);
+}
